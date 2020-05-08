55ca0d8bf3958a592235dbc086c3ae60c90e9242
Tiago Lima <tiago.lima@osf.digital>
2020-05-08 11:31:01 -0300

    First Commit.

---
+++ src/components/SideBar/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+export default class SideBar extends Component {
+  state = {
+    modules: [
+      {
+        id: 1,
+        title: "Iniciando com React",
+        lessons: [
+          { id: 1, title: "Primeira aula " },
+          { id: 2, title: "Segunda aula " },
+        ],
+      },
+      {
+        id: 2,
+        title: "Aprendendo Redux",
+        lessons: [
+          { id: 3, title: "Terceira aula " },
+          { id: 4, title: "Quarta aula " },
+        ],
+      },
+    ],
+  };
+
+  render() {
+    const { modules } = this.state;
+    return (
+      <aside>
+        {modules.map((module) => (
+          <div key={module.id}>
+            <strong>{module.title}</strong>
+            <ul>
+              {module.lessons.map((lesson) => (
+                <li key={lesson.id}>{lesson.title}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </aside>
+    );
+  }
+}
---
+++ src/components/Video/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Video extends Component {
+  render() {
+    return (
+      <div>
+        <strong>Módule X</strong>
+        <span>Lesson X</span>
+      </div>
+    );
+  }
+}
