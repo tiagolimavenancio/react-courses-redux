de032668a652b8d276af6e75bb20af63be8c6583
Tiago Lima <tiago.lima@osf.digital>
2020-05-08 11:46:07 -0300

    Started Redux. Created Store and Reducer.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Provider } from "react-redux";
 import SideBar from "./components/SideBar";
 import Video from "./components/Video";
+
+import store from "./store";
 
 function App() {
   return (
     <div className="App">
-      <Video />
-      <SideBar />
+      <Provider store={store}>
+        <Video />
+        <SideBar />
+      </Provider>
     </div>
   );
 }
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,42 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
+import { connect } from "react-redux";
 
-export default class SideBar extends Component {
-  state = {
-    modules: [
-      {
-        id: 1,
-        title: "Iniciando com React",
-        lessons: [
-          { id: 1, title: "Primeira aula " },
-          { id: 2, title: "Segunda aula " },
-        ],
-      },
-      {
-        id: 2,
-        title: "Aprendendo Redux",
-        lessons: [
-          { id: 3, title: "Terceira aula " },
-          { id: 4, title: "Quarta aula " },
-        ],
-      },
-    ],
-  };
+const SideBar = ({ modules }) => {
+  return (
+    <aside>
+      {modules.map((module) => (
+        <div key={module.id}>
+          <strong>{module.title}</strong>
+          <ul>
+            {module.lessons.map((lesson) => (
+              <li key={lesson.id}>{lesson.title}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </aside>
+  );
+};
 
-  render() {
-    const { modules } = this.state;
-    return (
-      <aside>
-        {modules.map((module) => (
-          <div key={module.id}>
-            <strong>{module.title}</strong>
-            <ul>
-              {module.lessons.map((lesson) => (
-                <li key={lesson.id}>{lesson.title}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </aside>
-    );
-  }
-}
+export default connect((state) => ({ modules: state }))(SideBar);
